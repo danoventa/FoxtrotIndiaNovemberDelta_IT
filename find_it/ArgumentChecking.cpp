4c5e197a9c36d6b58b96f8203267936c6859fd45
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2013-11-21 18:26:42 -0600

    Added information on what it's purpose is.

--- find_it/ArgumentChecking.cpp
+++ find_it/ArgumentChecking.cpp
@@ -1,3 +1,12 @@
+/* 
+	Will be used to check the arguments and parse through specific arguments. It 
+	should allow for the argumetns to be out of order, and for multiple arguments
+	to be used at the same time. Ideally this will not allow duplicate arguments, 
+	and potentially organize the arguments in some way. 
+
+	May or may not use the AdditionalAid class here. 
+*/
+	
 #include "ArgumentChecking.h"
 
 ArgumentChecking::ArgumentChecking(int argc, char** argv)
