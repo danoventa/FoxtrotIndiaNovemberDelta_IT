bdcbf18da35a9266357e3941f8aa3a2b317f3438
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2013-11-21 18:11:23 -0600

    Added all the expected classes.
    At least the basic classes that are requested for the project. More to
    come, if time permits.

---
+++ find_it/FindOperations.cpp
@@ -0,0 +1,131 @@
+#include "FindOperations.h"
+
+FindOperations::FindOperations()
+{
+
+}
+
+FindOperations::~FindOperations()
+{
+
+}
+
+void FindOperations::Summarize ()
+{
+
+}
+
+void FindOperations::Access()
+{
+
+}
+
+void FindOperations::Largest()
+{
+
+}
+
+void FindOperations::Smallest()
+{
+
+}
+
+void FindOperations::TreeDir()
+{
+
+}
+
+void FindOperations::Sparse()
+{
+
+}
+
+void FindOperations::Help()
+{
+
+}
+
+void FindOperations::Depth()
+{
+
+}
+
+void FindOperations::MaxDepth()
+{
+
+}
+
+void FindOperations::AMin()
+{
+
+}
+
+void FindOperations::ATime()
+{
+
+}
+
+void FindOperations::CMin()
+{
+
+}
+
+void FindOperations::CTime()
+{
+
+}
+
+void FindOperations::MMin()
+{
+
+}
+
+void FindOperations::MTime()
+{
+
+}
+
+void FindOperations::Links()
+{
+
+}
+
+void FindOperations::UID()
+{
+
+}
+
+void FindOperations::GID()
+{
+
+}
+
+void FindOperations::Size()
+{
+
+}
+
+void FindOperations::Type()
+{
+
+}
+
+void FindOperations::Print()
+{
+
+}
+
+void FindOperations::FPrint()
+{
+
+}
+
+void FindOperations::Exec()
+{
+
+}
+
+void FindOperations::OK()
+{
+
+}
---
+++ find_it/FindOperations.h
@@ -0,0 +1,43 @@
+#ifndef FINDOPERATIONS_H
+#define FINDOPERATIONS_H
+
+#include <iostream> 
+#include <string> 
+
+using namespace std;
+
+class FindOperations
+{
+public:
+	FindOperations();
+	~FindOperations();
+						// Operation Number:
+	void Summarize(); 	// 1
+	void Access();		// 2
+	void Largest();		// 3
+	void Smallest();	// 4
+	void TreeDir();  	// 5
+	void Sparse();		// 6
+	void Help();		// 7
+	void Depth();		// 8
+	void MaxDepth();	// 9
+	void AMin();		// 10
+	void ATime();		// 11
+	void CMin();		// 12
+	void CTime();		// 13
+	void MMin();		// 14
+	void MTime();		// 15
+	void Links();		// 16
+	void UID();			// 17
+	void GID();			// 18
+	void Size();		// 19
+	void Type();		// 20
+	void Print();		// 21
+	void FPrint();		// 22
+	void Exec();		// 23
+	void OK();			// 24
+private:
+
+};
+#endif //FINDOPERATIONS_H
+
